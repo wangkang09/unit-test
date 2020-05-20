cac8b0959eda760ae23efd6fd33c4a1b38513347
wangkang <wangkang_0902@163.com>
2020-05-20 17:11:39 +0800

    添加 powermock 测试用例

--- power-mockito-test/src/test/java/LuckyNumberGeneratorUnitTest.java
+++ power-mockito-test/src/test/java/LuckyNumberGeneratorUnitTest.java
@@ -3,12 +3,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.powermock.api.mockito.PowerMockito.*;
 
+/**
+ * 私有方法 mock！
+ */
+//此注解也可以开启 mockito 注解等功能
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(fullyQualifiedNames = "com.wangkang.test.*")
 public class LuckyNumberGeneratorUnitTest {
@@ -24,9 +30,14 @@ public class LuckyNumberGeneratorUnitTest {
         assert luckyNumberGenerator != null;
 
         LuckyNumberGenerator mock = spy(new LuckyNumberGenerator());
-        doReturn(300).when(mock,"getDefaultLuckyNumber");
+        when(mock, "getDefaultLuckyNumber").thenReturn(300);
         int result = mock.getLuckyNumber(null);
         assertEquals(300, result);
+
+        //mock 模式也行
+        mock = mock(LuckyNumberGenerator.class, InvocationOnMock::callRealMethod);
+        when(mock, "getDefaultLuckyNumber").thenReturn(301);
+        assertEquals(301, mock.getLuckyNumber(null));
     }
 
     /**
@@ -35,9 +46,11 @@ public class LuckyNumberGeneratorUnitTest {
     @Test
     public final void givenPrivateMethodWithArgumentAndReturn_whenUsingPowerMockito_thenCorrect() throws Exception {
         LuckyNumberGenerator mock = spy(new LuckyNumberGenerator());
-        doReturn(1).when(mock, "getComputedLuckyNumber", ArgumentMatchers.anyInt());
+        when(mock, "getComputedLuckyNumber", anyInt()).thenReturn(1);
         int result = mock.getLuckyNumber("Jack");
         assertEquals(1, result);
+
+        //同理 mock 模式也可以
     }
 
     /**
@@ -49,6 +62,7 @@ public class LuckyNumberGeneratorUnitTest {
         int result = mock.getLuckyNumber("Tyranosorous");
         verifyPrivate(mock).invoke("saveIntoDatabase", ArgumentMatchers.anyString());
         assertEquals(10000, result);
+        //同理 mock 模式也行
     }
 
 }
--- power-mockito-test/src/test/java/PowerMockitoUnitTest.java
+++ power-mockito-test/src/test/java/PowerMockitoUnitTest.java
@@ -5,24 +5,21 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.withSettings;
 import static org.powermock.api.mockito.PowerMockito.*;
 
 /**
- * @author wangkang
- * @date 2020-05-19
- * @since -
+ * mock 静态方法、final 方法
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(fullyQualifiedNames = "com.wangkang.test.*")
 public class PowerMockitoUnitTest {
-
     /**
      * 测试 mock 构造器，即 final 方法
      */
@@ -33,8 +30,7 @@ public class PowerMockitoUnitTest {
         CollaboratorWithFinalMethods collaborator = new CollaboratorWithFinalMethods();
         //验证调用了无参
         verifyNew(CollaboratorWithFinalMethods.class).withNoArguments();
-
-        doReturn("Hello Baeldung!").when(collaborator).helloMethod();
+        when(collaborator.helloMethod()).thenReturn("Hello Baeldung!");
         String welcome = collaborator.helloMethod();
         //验证调用
         verify(collaborator).helloMethod();
@@ -47,17 +43,25 @@ public class PowerMockitoUnitTest {
      */
     @Test
     public void givenStaticMethods_whenUsingPowerMockito_thenCorrect() {
-        mockStatic(CollaboratorWithStaticMethods.class);
+        //mock
+        mockStatic(CollaboratorWithStaticMethods.class, withSettings().defaultAnswer(InvocationOnMock::callRealMethod));
+        //以上相当于：spy(CollaboratorForPartialMocking.class);
 
+        //stubbing
         when(CollaboratorWithStaticMethods.firstMethod(Mockito.anyString())).thenReturn("Hello Baeldung!");
         when(CollaboratorWithStaticMethods.secondMethod()).thenReturn("Nothing special");
-        when(CollaboratorWithStaticMethods.thirdMethod()).thenThrow(new RuntimeException("dd"));
+        //callRealMethod！！
+        assert CollaboratorWithStaticMethods.thirdMethod().equals("Hello no one again!");
+        when(CollaboratorWithStaticMethods.thirdMethod()).thenReturn("thirdFirstReturn", "thirdSecondReturn").thenThrow(new RuntimeException("dd"));
 
         String firstWelcome1 = CollaboratorWithStaticMethods.firstMethod("dds");
         String firstWelcome2 = CollaboratorWithStaticMethods.secondMethod();
         assertEquals("Hello Baeldung!", firstWelcome1);
         assertEquals("Nothing special", firstWelcome2);
 
+        //验证串联式打桩声明
+        assert CollaboratorWithStaticMethods.thirdMethod().equals("thirdFirstReturn");
+        assert CollaboratorWithStaticMethods.thirdMethod().equals("thirdSecondReturn");
         try {
             CollaboratorWithStaticMethods.thirdMethod();
             Assert.fail();
@@ -68,19 +72,21 @@ public class PowerMockitoUnitTest {
 
     @Test
     public void givenPartialMocking_whenUsingPowerMockito_thenCorrect() throws Exception {
+        //spy static
         String returnValue;
-
         spy(CollaboratorForPartialMocking.class);
         when(CollaboratorForPartialMocking.staticMethod()).thenReturn("I am a static mock method.");
         returnValue = CollaboratorForPartialMocking.staticMethod();
-        CollaboratorForPartialMocking.staticMethod();
         assertEquals("I am a static mock method.", returnValue);
 
+        //spy 方法得到 mock 对象必须传入一个创建的实例
+        //spy final、private 方法
         CollaboratorForPartialMocking collaborator = new CollaboratorForPartialMocking();
         CollaboratorForPartialMocking mock = spy(collaborator);
 
         when(mock.finalMethod()).thenReturn("I am a final mock method.");
         returnValue = mock.finalMethod();
+        //验证调用了 finalMethod 方法
         verify(mock).finalMethod();
         assertEquals("I am a final mock method.", returnValue);
 
